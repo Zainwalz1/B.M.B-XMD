38530be98737cea586661a01ac81c3308b91dcaf
Zainwalz1 <mleyacollen@gmail.com>
2025-05-08 07:57:43 +0200

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "B.M.B-XMD~BzVBxL7T#mIDmWsnM4h2E97QP8q4_8fdM2utM25yKoMN7Y5yvFH4",
+SESSION_ID: process.env.SESSION_ID || "B.M.B-XMD~YyElxJbJ#QiefdIZJl9qPJoR1kMnQa6HBQHcDoBAKtv5nf0HPE8g",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -17,7 +17,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "25566862457",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263776030359",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "✦ B.M.B-XMD ✦ XMD ✦",
 // add bot owner name
